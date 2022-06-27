4641057f04bba5ec817dd5e716ada6ab60fac8b5
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-06-27 22:31:42 +0900

    ゲストログイン追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get 'about', to: "home#about"
   resources :posts
   get '/users/account', to: 'users#account', as: :account
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
