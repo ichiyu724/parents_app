beedc64eb008ee892e363550dda360b3ef891319
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-07-30 21:58:20 +0900

    新規登録画面にユーザー名を追加、アカウント情報変更画面を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def after_sign_in_path_for(resource)
         user_path(@user)
-      end
+    end
     
-      private
-          def sign_in_required
-              redirect_to new_user_session_url unless user_signed_in?
-          end
+    private
+
+    def sign_in_required
+      redirect_to new_user_session_url unless user_signed_in?
+    end
+
+    def configure_permitted_parameters
+      added_attrs = [ :email, :username, :password, :password_confirmation ]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: [added_attrs, :profile, :icon_image]
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    end
 end
