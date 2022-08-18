45f9b2801d70ab99f00807460a9ea8bd97babd0d
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-18 10:38:54 +0900

    ログイン時のバリデーションのテストコードを記載

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'hogehoge@example.com' }
+    username { 'hoge' }
+    password { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+
+  describe "ログイン" do
+    it "emailとパスワードのバリデーションが設定できていること" do
+    expect(user.valid?).to eq(true)
+    end
+  end
 end
