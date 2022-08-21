179024a8c656a3e995c8ed61f3556c261238d03d
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-22 00:01:57 +0900

    relationship_spec.rbにフォロー、フォロー解除の検証を導入

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,7 @@
 class Relationship < ApplicationRecord
     belongs_to :following, class_name: 'User'
     belongs_to :follower, class_name: 'User'
+
+    validates :following_id, presence: true, uniqueness: { scope: :follower_id }
+    validates :follower_id, presence: true
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -3,5 +3,13 @@ require 'rails_helper'
 RSpec.describe Relationship, type: :model do
   let(:relationship) { FactoryBot.create(:relationship) }
 
-  
+  describe 'フォロー、フォロー解除の検証' do
+
+    context "following_idとfolower_idがともに保存できる場合" do
+      it "全てのパラメーターが揃っていれば保存できる" do
+        expect(relationship).to be_valid
+      end
+    end
+
+  end
 end
