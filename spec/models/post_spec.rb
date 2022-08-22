770f494c856718a4d3c2d2f060e9fff41f506dee
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-22 23:50:12 +0900

    child_sexのテストデータを、配列からsampleメソッドでランダムに取得する形で作成

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-
   let(:post) { create(:post) }
+
+  before do
+    gender_select = ["未選択", "男の子", "女の子"]
+    post.child_sex = gender_select.sample
+  end
 
     it "新規投稿のバリデーションが設定できていること" do
       expect(post.valid?).to eq(true)
