523015d1aef8072f9308adb668db7d476c799685
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-23 15:53:31 +0900

    favoriteモデルのバリデーション、user_idとpost_idがあれば保存できることのテスト

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @favorite =  FactoryBot.build(:favorite)
+    @user = FactoryBot.create(:user)
+  end
+
+  describe '正常値と異常値の確認' do
+    context 'favoriteモデルのバリデーション' do
+      it "user_idとpost_idがあれば保存できる" do
+        expect(FactoryBot.create(:favorite, user_id: @user.id)).to be_valid
+      end
+
+    end
+  end
 end
