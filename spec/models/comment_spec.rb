793a5cf1dc6b4141e4e118973bce3e0f2fc4e2af
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-23 23:42:55 +0900

    commentのpost_idとuser_idの有無による保存の可否をテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   let(:user) { create(:user) }
+  let(:post) { create(:post) }
   let(:comment) {create(:comment, user_id: user.id)}
 
   describe "コメント投稿" do
@@ -16,6 +17,22 @@ RSpec.describe Comment, type: :model do
         comment.body = ""
         comment.valid?
         expect(comment.errors.full_messages).to include("コメントを入力してください")
+      end
+    end
+
+    context "user_idとpost_idの保存" do
+      it "user_idとpost_idがあれば保存できる" do
+        expect(FactoryBot.create(:comment, user_id: user.id, post_id: post.id)).to be_valid
+      end
+
+      it "user_idがなければ無効な状態であること" do
+        comment.user_id = nil
+        expect(comment).to be_invalid
+      end
+
+      it "post_idがなければ無効な状態であること" do
+        comment.post_id = nil
+        expect(comment).to be_invalid
       end
     end
   end
