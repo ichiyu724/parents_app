60e3a7d505f288e03447792c72e3d9e39f8ee8ca
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-23 22:29:18 +0900

    コメント投稿のバリデーションを検証

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:comment) {create(:comment, user_id: user.id)}
+
+  describe "コメント投稿" do
+    context "コメント投稿成功" do
+      it "コメント投稿のバリデーションが設定できていること" do
+        expect(comment.valid?).to eq(true)
+      end
+    end
+
+    context "コメント投稿失敗" do
+      it "コメント内容が空欄だと投稿できない" do
+        comment.body = ""
+        comment.valid?
+        expect(comment.errors.full_messages).to include("コメントを入力してください")
+      end
+    end
+  end
 end
