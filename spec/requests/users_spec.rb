89461ff16643ceab1be514e1efd76f0cbbd7b04e
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-25 23:09:18 +0900

    userのrequestスペックを記述。ユーザー一覧が表示できるようテスト

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  let!(:user) { create(:user) }
+  let!(:post) { create(:post, user_id: user.id) }
+  
+  describe "GET /users" do
+    it "ユーザー一覧が表示できること" do
+      sign_in user
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
