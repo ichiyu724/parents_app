1b8b5843a95e292b7872bb6ba17f23f3a12d1470
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-26 00:02:46 +0900

    投稿一覧というタイトルが表示されていることのテスト

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,11 +4,18 @@ RSpec.describe "Users", type: :request do
   let!(:user) { create(:user) }
   let!(:post) { create(:post, user_id: user.id) }
   
-  describe "GET /users" do
-    it "ユーザー一覧が表示できること" do
+  describe "GET #index" do
+    before do 
       sign_in user
       get users_path
+    end
+    it "ユーザー一覧が表示できること" do
       expect(response).to have_http_status(200)
     end
+
+    it 'タイトルが正しく表示されていること' do
+      expect(response.body).to include("ユーザー一覧")
+    end
+
   end
 end
