cab640c422d0c1b61c77f37eb03db8643ca8df82
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-26 12:19:29 +0900

    ログインしていないユーザーがマイページにアクセスできず、ログイン画面にリダイレクトされることの検証_index

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,36 +6,52 @@ RSpec.describe "Users", type: :request do
   let(:post) { create(:post, user_id: user.id) }
   
   describe "GET #index" do
-    before do 
-      sign_in user
-      get users_path
-    end
-    it "ユーザー一覧が表示できること" do
-      expect(response).to have_http_status(200)
+    context "ログイン中のユーザー" do
+      before do 
+        sign_in user
+        get users_path
+      end
+
+      it "ユーザー一覧が表示できること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'タイトルが正しく表示されていること' do
+        expect(response.body).to include("ユーザー一覧")
+      end
+
+      it "ユーザー一覧にログイン中のuserが含まれていないこと" do
+        user1 = FactoryBot.create(:user)
+        sign_in(user1)
+        expect(response).not_to include user1.id.to_s
+      end
     end
 
-    it 'タイトルが正しく表示されていること' do
-      expect(response.body).to include("ユーザー一覧")
-    end
+    context do
+      before do
+        get users_path
+      end
+      it "マイページにアクセスできないこと" do
+        expect(response).to have_http_status(302)
+      end
 
-    it "ユーザー一覧にログイン中のuserが含まれていないこと" do
-      user1 = FactoryBot.create(:user)
-      sign_in(user1)
-      expect(response).not_to include user1.id.to_s
+      it "ログイン画面にリダイレクトされること" do
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 
   describe "GET #show" do
     context "ログイン中のユーザー" do
-      it "マイページが表示できること" do
+      before do
         sign_in user
         get user_path(user)
+      end
+      it "マイページが表示できること" do
         expect(response).to have_http_status(200)
       end
 
       it "プロフィールの見出し名が表示できること" do
-        sign_in user
-        get user_path(user)
         expect(response.body).to include "プロフィール"
         expect(response.body).to include "#{user.username}さんの投稿"
         expect(response.body).to include "お気に入りした投稿"
@@ -43,13 +59,15 @@ RSpec.describe "Users", type: :request do
     end
 
     context "ログインユーザーでないユーザー" do
-      it "マイページにアクセスできないこと" do
+      before do
         get user_path(user)
+      end
+      
+      it "マイページにアクセスできないこと" do
         expect(response).to have_http_status(302)
       end
 
       it "ログイン画面にリダイレクトされること" do
-        get user_path(user)
         expect(response).to redirect_to "/users/sign_in"
       end
 
