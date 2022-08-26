36448fd2bc02989f4c52f4e27cfb811ebb4beaa3
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-08-26 14:01:57 +0900

    user/editコントローラー、ログイン中の時にレスポンスが正しいことをテスト

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Users", type: :request do
   let(:other_user) { create(:user) }
   let(:post) { create(:post, user_id: user.id) }
   
-  describe "GET #index" do
+  describe "#index" do
     context "ログイン中のユーザー" do
       before do 
         sign_in user
@@ -31,6 +31,7 @@ RSpec.describe "Users", type: :request do
       before do
         get users_path
       end
+
       it "マイページにアクセスできないこと" do
         expect(response).to have_http_status(302)
       end
@@ -41,7 +42,7 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  describe "GET #show" do
+  describe "#show" do
     context "ログイン中のユーザー" do
       before do
         sign_in user
@@ -70,7 +71,23 @@ RSpec.describe "Users", type: :request do
       it "ログイン画面にリダイレクトされること" do
         expect(response).to redirect_to "/users/sign_in"
       end
+    end
+  end
 
+  describe "#edit" do
+    context "ログイン中のユーザー" do
+      before do 
+        sign_in user
+        get edit_user_path(user)
+      end
+
+      it "ユーザー編集ページが表示されること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'プロフィール編集の見出し名が正しく表示されていること' do
+        expect(response.body).to include("プロフィール編集")
+      end
     end
   end
 end
