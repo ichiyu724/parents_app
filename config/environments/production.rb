c32f6d923b3b9f96f3d43b5008aa99422a5778eb
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-09-20 01:36:01 +0900

    パスワード再設定メールを送れるよう、production.rbを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,6 +99,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'parents-link.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV["GOOGLE_MAIL_ADDRESS"],
+    :password => ENV["GOOGLE_MAILER_PASSWORD"],
+    :authentication => 'login'
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
