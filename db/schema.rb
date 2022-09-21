54d2b4ad4e4942aa8c9949ad7eab0d06a810ac5f
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-09-21 14:53:26 +0900

    mysqlとwebpackのエラーが出たためschema.rbを修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2022_08_02_113618) do
 
-  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.text "body"
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2022_08_02_113618) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "favorites", charset: "utf8mb4", force: :cascade do |t|
+  create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2022_08_02_113618) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
     t.string "child_sex"
@@ -43,14 +43,14 @@ ActiveRecord::Schema.define(version: 2022_08_02_113618) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "relationships", charset: "utf8mb4", force: :cascade do |t|
+  create_table "relationships", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.integer "following_id", null: false
     t.integer "follower_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
