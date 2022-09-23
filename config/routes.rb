30968c62af140ba57d1b04e0caf579a89232b2de
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-09-23 23:57:17 +0900

    GoogleMapのAPIを取得し、マップをhtmlファイルに表示

--- config/routes.rb
+++ config/routes.rb
@@ -32,5 +32,7 @@ Rails.application.routes.draw do
   resources :posts , expect: [:index] do
     resource :favorites, only: [:create, :destroy]
   end
+
+  resources :maps, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
