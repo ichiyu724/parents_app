5006d0ccb39a5b4aedaab4804c9d248df53340e3
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-10-13 01:07:17 +0900

    childのindexが正常にレスポンスされていることのrequestスペックを記述

--- spec/requests/children_spec.rb
+++ spec/requests/children_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Children", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:child) { create(:child) }
+  let(:user) { create(:user) }
+  
+  describe "#index" do
+    context "ログイン中のユーザー" do
+      before do 
+        sign_in user
+        get user_children_path(user_id: user.id)
+      end
+    
+      it "子供の一覧が表示できること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'タイトルが正しく表示されていること' do
+        expect(response.body).to include("お子さん一覧")
+      end
+    end
   end
 end
