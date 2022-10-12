5ee3efd831e453f7bce2b8c8ed0bbcffc5afd75a
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-10-13 01:25:08 +0900

    childrenのcreateアクションが成功することのテストを記述

--- spec/requests/children_spec.rb
+++ spec/requests/children_spec.rb
@@ -64,5 +64,25 @@ RSpec.describe "Children", type: :request do
         expect(response).to redirect_to "/users/sign_in"
       end
     end
+
+    describe "#create" do
+      context "パラメータが正常な場合" do
+        before do
+          sign_in user
+        end
+        it "子供の登録が正常に完了するか" do
+          expect do
+            post user_children_path(user_id: user.id), params: { child: FactoryBot.attributes_for(:child) }
+          end.to change(user.children, :count).by(1)
+        end
+  
+        it "投稿成功後、リダイレクトするか" do
+          post user_children_path(user_id: user.id), params: { child: FactoryBot.attributes_for(:child) }
+          expect(response).to redirect_to user_children_path(user_id: user.id)
+        end
+      end
+  
+      
+    end
   end
 end
