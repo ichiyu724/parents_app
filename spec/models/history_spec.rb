76b0ee6708bb94c2c02f66081842ed94001a7858
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-10-13 00:42:03 +0900

    historyのmodelスペックを記述

--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe History, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:history) { create(:history) }
+
+  describe "接種日登録" do
+    context "登録成功" do
+      it "接種日のバリデーションが設定できていること" do
+        expect(history.valid?).to eq(true)
+      end
+    end
+
+    context "登録失敗" do
+      it "接種日が空欄だと投稿できない" do
+        history.date = ""
+        history.valid?
+        expect(history.errors.full_messages).to include("接種日を入力してください")
+      end
+    end
+  end
 end
