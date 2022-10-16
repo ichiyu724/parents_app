e106f568012c081d12180bd418d8f72b75b57328
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-10-16 19:49:31 +0900

    CIを走らせたときのrubocopの指摘事項を修正

--- app/models/child.rb
+++ app/models/child.rb
@@ -8,12 +8,12 @@ class Child < ApplicationRecord
   def pre_school_year(birthdate)
     fifth_birthday = birthdate + 5.years
     year =  case fifth_birthday.month
-              when 1..3
-                fifth_birthday.year
-              when 4
-                fifth_birthday.day == 1 ? fifth_birthday.year : fifth_birthday.year.next
-              when 5..12
-                sfifth_birthday.year.next
+            when 1..3
+              fifth_birthday.year
+            when 4
+              fifth_birthday.day == 1 ? fifth_birthday.year : fifth_birthday.year.next
+            when 5..12
+              sfifth_birthday.year.next
             end
     Date.new(year, 4, 1)..Date.new(year.next, 3, 31)
   end
